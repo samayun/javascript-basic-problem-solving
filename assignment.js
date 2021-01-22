d8ff8c5f0764113fa07afec6af15b87883ae9bc7
Samayun Chowdhury <samayunmc99@gmail.com>
2021-01-22 16:42:45 +0600

    kilometerToMeter & budgetCalculator problem solved

---
+++ assignment.js
@@ -0,0 +1,44 @@
+// https://github.com/samayun/javascript-basic-problem-solving
+
+/**
+ * Kilometer to Meter Unit Conversion
+ * @param Number km
+ * @return Number meter
+ */
+function kilometerToMeter(km) {
+    if (typeof km != "number") {
+        return "Please Input Number Please! ";
+    }
+    if (km <= 0) {
+        return "Please Input Positive Unit";
+    }
+    // 1km = 1000 meter
+    return km * 1000;
+}
+
+/**
+ * Budget Calculate for electronics store
+ * @param Number clockQuantity , Number phoneQuantity , Number laptopQuantity ,
+ * @return Number totalCost
+ */
+function budgetCalculator(clockQuantity , phoneQuantity, laptopQuantity ) {
+    // Validation
+    if (typeof clockQuantity != "number" || typeof phoneQuantity != "number" || typeof laptopQuantity != "number" )
+    {
+        return "Wrong Input! Please Provide all credentials properly";
+    }
+    if (clockQuantity <= 0 ||  phoneQuantity <= 0 ||  laptopQuantity <= 0 )
+    {
+        return "Wrong Input! Please Provide valid quantity minimum 1 quantity";
+    }
+
+    const clockPrice = clockQuantity * 50; // 1 clock = 50TK
+    const phonePrice = phoneQuantity * 100; // 1 phone = 100TK
+    const laptopPrice = laptopQuantity * 500; // 1 laptop = 500TK
+    
+    let totalCost = clockPrice + phonePrice + laptopPrice;
+    return totalCost;
+}
+
+
+
